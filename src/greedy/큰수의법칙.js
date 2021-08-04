c3bed92af486dd76acae2574ed48e33bff6b0f41
mhkim <yahma25@gmail.com>
2021-08-04 23:59:55 +0900

    #2 가장 큰 수 연속적으로 합산 후, 두 번째로 큰 수 한 번만 더하는 식으로 개선

--- src/greedy/큰수의법칙.js
+++ src/greedy/큰수의법칙.js
@@ -6,6 +6,35 @@
  * @returns
  */
 export function answer(m, k, nums) {
+  const ascNums = nums.sort((a, b) => b - a);
+
+  let result = 0;
+  let repeat = 0;
+  const firstMax = ascNums[0];
+  const secondMax = ascNums[1];
+
+  for (let i = 0; i < m; i++) {
+    if (repeat === k) {
+      result += secondMax;
+      repeat = 0;
+    } else {
+      result += firstMax;
+    }
+    repeat++;
+  }
+
+  return result;
+}
+
+/**
+ * 최초 풀이
+ *
+ * 개선 사항
+ * - 가장 큰 값, 두 번째로 큰 값을 변수에 세팅해놓고,
+ * - 가장 큰 값의 반복 횟수가 끝나면 두 번째로 큰 값을 한 번만 더해주면 됐엇음.
+ */
+/*
+export function answer(m, k, nums) {
   const sortedNums = nums.sort((a, b) => b - a);
 
   let result = 0;
@@ -25,3 +54,4 @@ export function answer(m, k, nums) {
 
   return result;
 }
+*/
