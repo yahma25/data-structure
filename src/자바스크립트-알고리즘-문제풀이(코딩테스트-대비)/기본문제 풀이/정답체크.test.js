1b3ff01abd32afed73860deb83e50aeefeb65efd
mhkim <yahma25@gmail.com>
2021-09-20 17:37:36 +0900

    [수정] 섹션 1 전체 테스트를 한 번에 할 수 있도록 변경

--- src/자바스크립트-알고리즘-문제풀이(코딩테스트-대비)/기본문제 풀이/정답체크.test.js
+++ src/자바스크립트-알고리즘-문제풀이(코딩테스트-대비)/기본문제 풀이/정답체크.test.js
@@ -1,6 +1,8 @@
 import { 세수중최솟값, 세수중최솟값_Math_Min } from "./세수중최솟값";
 
-test("세수중최솟값", () => {
-  expect(세수중최솟값(6, 5, 11)).toBe(5);
-  expect(세수중최솟값_Math_Min(6, 5, 11)).toBe(5);
+describe("섹션 1 결과 확인", () => {
+  test("세수중최솟값", () => {
+    expect(세수중최솟값(6, 5, 11)).toBe(5);
+    expect(세수중최솟값_Math_Min(6, 5, 11)).toBe(5);
+  });
 });
