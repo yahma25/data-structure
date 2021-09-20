2e4d09c6e57d5fa5fa4849dd5e0f984be16d553c
mhkim <yahma25@gmail.com>
2021-09-21 00:38:36 +0900

    [추가] 1부터N까지합출력하기

--- src/자바스크립트-알고리즘-문제풀이(코딩테스트-대비)/기본문제 풀이/정답체크.test.js
+++ src/자바스크립트-알고리즘-문제풀이(코딩테스트-대비)/기본문제 풀이/정답체크.test.js
@@ -1,6 +1,7 @@
 import { 세수중최솟값, 세수중최솟값_Math_Min } from './세수중최솟값';
 import { 삼각형판별하기, 삼각형판별하기_math_max } from './삼각형판별하기';
 import { 연필개수 } from './연필개수';
+import { solution_1부터N까지합출력하기 } from './1부터N까지합출력하기';
 
 describe('섹션 1 결과 확인', () => {
   test('세수중최솟값', () => {
@@ -19,5 +20,10 @@ describe('섹션 1 결과 확인', () => {
   test('연필개수', () => {
     expect(연필개수(25)).toBe(3);
     expect(연필개수(178)).toBe(15);
+  });
+
+  test('1부터N까지합출력하기', () => {
+    expect(solution_1부터N까지합출력하기(6)).toBe(21);
+    expect(solution_1부터N까지합출력하기(10)).toBe(55);
   });
 });
