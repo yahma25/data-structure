899492b020fc1a9c40983158d7b99621e6bd3ccd
mhkim <yahma25@gmail.com>
2021-09-21 00:30:20 +0900

    [추가] 연필개수

--- src/자바스크립트-알고리즘-문제풀이(코딩테스트-대비)/기본문제 풀이/정답체크.test.js
+++ src/자바스크립트-알고리즘-문제풀이(코딩테스트-대비)/기본문제 풀이/정답체크.test.js
@@ -1,5 +1,6 @@
 import { 세수중최솟값, 세수중최솟값_Math_Min } from "./세수중최솟값";
 import { 삼각형판별하기, 삼각형판별하기_math_max } from "./삼각형판별하기";
+import { 연필개수 } from "./연필개수";
 
 describe("섹션 1 결과 확인", () => {
   test("세수중최솟값", () => {
@@ -13,5 +14,10 @@ describe("섹션 1 결과 확인", () => {
 
     expect(삼각형판별하기_math_max(6, 7, 11)).toBe("YES");
     expect(삼각형판별하기_math_max(13, 33, 17)).toBe("NO");
+  });
+
+  test("연필개수", () => {
+    expect(연필개수(25)).toBe(3);
+    expect(연필개수(178)).toBe(15);
   });
 });
