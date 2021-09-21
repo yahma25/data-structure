9126b3f0ed6ee698aed18ee61d79c5107d1e65eb
mhkim <yahma25@gmail.com>
2021-09-21 20:11:33 +0900

    [추가] 보이는 학생

--- src/자바스크립트-알고리즘-문제풀이(코딩테스트-대비)/Section-2-1,2차원-배열-탐색/정답체크.test.js
+++ src/자바스크립트-알고리즘-문제풀이(코딩테스트-대비)/Section-2-1,2차원-배열-탐색/정답체크.test.js
@@ -1,8 +1,14 @@
 import { solution_큰수출력하기, solution_큰수출력하기_math_max } from './1.큰수출력하기';
+import { solution_보이는학생, solution_보이는학생_reduce } from './2.보이는학생';
 
 describe('섹션 2 결과 확인', () => {
   test('큰수출력하기', () => {
     expect(solution_큰수출력하기([7, 3, 9, 5, 6, 12])).toStrictEqual([7, 9, 6, 12]);
     expect(solution_큰수출력하기_math_max([7, 3, 9, 5, 6, 12])).toStrictEqual([7, 9, 6, 12]);
+  });
+
+  test('보이는학생', () => {
+    expect(solution_보이는학생([130, 135, 148, 140, 145, 150, 150, 153])).toBe(5);
+    expect(solution_보이는학생_reduce([130, 135, 148, 140, 145, 150, 150, 153])).toBe(5);
   });
 });
