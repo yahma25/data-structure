5de20a232b17abac516423d9b27a3b7770ab9324
mhkim <yahma25@gmail.com>
2021-09-22 10:38:59 +0900

    [추가] 숫자만 추출

--- src/인프런-자바스크립트-알고리즘-문제풀이/Section-3-문자열-탐색/정답체크.test.js
+++ src/인프런-자바스크립트-알고리즘-문제풀이/Section-3-문자열-탐색/정답체크.test.js
@@ -1,5 +1,6 @@
 import { solution_회문문자열, solution_회문문자열_reverse } from './1.회문문자열';
 import { solution_유효한팰린드롬, solution_유효한팰린드롬_optimization } from './2.유효한팰린드롬';
+import { solution_숫자만추출, solution_숫자만추출_regex } from './3.숫자만추출';
 
 describe('섹션 3 결과 확인', () => {
   test('회문문자열', () => {
@@ -20,5 +21,10 @@ describe('섹션 3 결과 확인', () => {
     expect(solution_유효한팰린드롬_optimization('found7, time: study; Yduts; emit, 7Dnuo')).toBe(
       'NO'
     );
+  });
+
+  test('숫자만추출', () => {
+    expect(solution_숫자만추출('g0en2T0s8eSoft')).toBe(208);
+    expect(solution_숫자만추출_regex('g0en2T0s8eSoft')).toBe(208);
   });
 });
