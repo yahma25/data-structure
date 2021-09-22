7827cb7ca02b03df1b020a1bbb70f0c4a626e191
mhkim <yahma25@gmail.com>
2021-09-22 09:54:33 +0900

    [추가] 유효한 팰린드롬

---
+++ src/인프런-자바스크립트-알고리즘-문제풀이/Section-3-문자열-탐색/2.유효한팰린드롬.js
@@ -0,0 +1,42 @@
+/*
+유효한 팰린드롬
+앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 합니다.
+문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력하는 프로그램을 
+작성하세요.
+단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.
+알파벳 이외의 문자들의 무시합니다.
+▣ 입력설명
+첫 줄에 정수 길이 100을 넘지 않는 공백이 없는 문자열이 주어집니다. 
+▣ 출력설명
+첫 번째 줄에 팰린드롬인지의 결과를 YES 또는 NO로 출력합니다.
+▣ 입력예제 1 
+found7, time: study; Yduts; emit, 7Dnuof
+▣ 출력예제 1
+YES
+*/
+export function solution_유효한팰린드롬(s) {
+  let answer = 'YES';
+
+  const onlyAlphabet = s.replace(/[^a-z|A-Z|0-9]/g, '').toLowerCase();
+  const mid = Math.floor(s.length / 2);
+
+  for (let i = 0; i < mid; i++) {
+    if (onlyAlphabet[i] !== onlyAlphabet[onlyAlphabet.length - 1 - i]) {
+      answer = 'NO';
+    }
+  }
+
+  return answer;
+}
+
+export function solution_유효한팰린드롬_optimization(s) {
+  let answer = 'YES';
+
+  const onlyAlphabet = s.replace(/[^a-z|A-Z|0-9]/g, '').toLowerCase();
+  const onlyAlphabetReversed = onlyAlphabet.split('').reverse().join('');
+
+  if (onlyAlphabet !== onlyAlphabetReversed) {
+    answer = 'NO';
+  }
+  return answer;
+}
--- src/인프런-자바스크립트-알고리즘-문제풀이/Section-3-문자열-탐색/정답체크.test.js
+++ src/인프런-자바스크립트-알고리즘-문제풀이/Section-3-문자열-탐색/정답체크.test.js
@@ -1,4 +1,5 @@
 import { solution_회문문자열, solution_회문문자열_reverse } from './1.회문문자열';
+import { solution_유효한팰린드롬, solution_유효한팰린드롬_optimization } from './2.유효한팰린드롬';
 
 describe('섹션 3 결과 확인', () => {
   test('회문문자열', () => {
@@ -7,5 +8,17 @@ describe('섹션 3 결과 확인', () => {
 
     expect(solution_회문문자열_reverse('gooG')).toBe('YES');
     expect(solution_회문문자열_reverse('bad')).toBe('NO');
+  });
+
+  test('유효한팰린드롬', () => {
+    expect(solution_유효한팰린드롬('found7, time: study; Yduts; emit, 7Dnuof')).toBe('YES');
+    expect(solution_유효한팰린드롬('found7, time: study; Yduts; emit, 7Dnuo')).toBe('NO');
+
+    expect(solution_유효한팰린드롬_optimization('found7, time: study; Yduts; emit, 7Dnuof')).toBe(
+      'YES'
+    );
+    expect(solution_유효한팰린드롬_optimization('found7, time: study; Yduts; emit, 7Dnuo')).toBe(
+      'NO'
+    );
   });
 });
