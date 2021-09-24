e564a8a9faf01b3c9527ca02de12856554b03fff
mhkim <yahma25@gmail.com>
2021-09-24 21:21:08 +0900

    [추가] 연속 부분수열 1

--- src/인프런-자바스크립트-알고리즘-문제풀이/Section-5-효율성/정답체크.test.js
+++ src/인프런-자바스크립트-알고리즘-문제풀이/Section-5-효율성/정답체크.test.js
@@ -1,5 +1,6 @@
 import { solution_두배열합치기 } from './1.두배열합치기';
 import { solution_공통원소구하기 } from './2.공통원소구하기';
+import { solution_연속부분수열1 } from './3.연속부분수열1';
 
 describe('섹션 5 결과 확인', () => {
   test('두배열합치기', () => {
@@ -16,5 +17,11 @@ describe('섹션 5 결과 확인', () => {
 
   test('공통원소구하기', () => {
     expect(solution_공통원소구하기([1, 3, 9, 5, 2, 11], [3, 2, 5, 7, 8])).toStrictEqual([2, 3, 5]);
+  });
+
+  test('연속부분수열1', () => {
+    expect(solution_연속부분수열1(6, [1, 2, 1, 3, 1, 1, 1, 2])).toBe(3);
+    expect(solution_연속부분수열1(6, [1, 1, 1, 2, 4])).toBe(1);
+    expect(solution_연속부분수열1(2, [1, 1, 1])).toBe(2);
   });
 });
