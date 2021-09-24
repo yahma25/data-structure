546adde9b88d3dab32746334e7f34d42c937c019
mhkim <yahma25@gmail.com>
2021-09-24 23:28:50 +0900

    [추가] 최대 매출

--- src/인프런-자바스크립트-알고리즘-문제풀이/Section-5-효율성/정답체크.test.js
+++ src/인프런-자바스크립트-알고리즘-문제풀이/Section-5-효율성/정답체크.test.js
@@ -2,6 +2,7 @@ import { solution_두배열합치기 } from './1.두배열합치기';
 import { solution_공통원소구하기 } from './2.공통원소구하기';
 import { solution_연속부분수열1 } from './3.연속부분수열1';
 import { solution_연속부분수열2, solution_연속부분수열2_one_while } from './4.연속부분수열2';
+import { solution_최대매출 } from './5.최대매출';
 
 describe('섹션 5 결과 확인', () => {
   test('두배열합치기', () => {
@@ -29,5 +30,9 @@ describe('섹션 5 결과 확인', () => {
   test('연속부분수열2', () => {
     expect(solution_연속부분수열2(5, [1, 3, 1, 2, 3])).toBe(10);
     expect(solution_연속부분수열2_one_while(5, [1, 3, 1, 2, 3])).toBe(10);
+  });
+
+  test('최대매출', () => {
+    expect(solution_최대매출(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15])).toBe(56);
   });
 });
